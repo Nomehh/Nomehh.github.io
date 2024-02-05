86e5f543999ce72a73e6e138688575211b75000d
Mathieu PERRIOT <mathperriot@gmail.com>
2024-02-06 00:06:30 +0100

    :beers: I'm stupid

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible-section');
+      console.log(entry.target.id,'visible');
     }
     else {
         entry.target.classList.remove('visible-section');
+        console.log(entry.target.id, 'invisible');
     }
   });
 });
