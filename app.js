8379e3afbfe08bbb40121cf8d4629d0991415864
Mathieu PERRIOT <mathperriot@gmail.com>
2024-02-06 00:01:52 +0100

    :beers: Fix

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
+      entry.target.classList.add('visible-section');
     }
     else {
-        entry.target.classList.remove('visible');
+        entry.target.classList.remove('visible-section');
     }
   });
 });
 
-const hiddenSections = document.querySelectorAll('.hidden');
+const hiddenSections = document.querySelectorAll('.hidden-section');
 hiddenSections.forEach((section) => {
     observer.observe(section);
     }
